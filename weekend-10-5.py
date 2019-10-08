ec5d826dbdc18eeddf6d9aff0556d7058c5944d9
Serdar Durbayev <serdardurbayev@Serdars-MacBook-Pro.local>
2019-10-08 13:53:17 -0400

    Fix bugs

--- weekend-10-5.py
+++ weekend-10-5.py
@@ -160,11 +160,40 @@ print(deque(c))
         # If you have open-source projects that use Git, then GitHub is designed to help you better manage them.
 
 # 3.Answer the following question:
-# PR is Version Control Systems
-# Fork VS Commit
-# Init VS Clone
-# Branching and Merging
 
+# 3.1 PR is Version Control Systems
+#Pull requests let you tell others about changes you've pushed to a branch in a repository on GitHub. 
+#Once a pull request is opened, you can discuss and review the potential changes with collaborators and
+#add follow-up commits before your changes are merged into the base branch.
+
+#3.2 Fork VS Commit
+#Commit is record changes to the repository.
+
+#Forking
+#A fork is a copy of a repository that allows you to freely experiment with changes without
+#  affecting the original project. A forked repository differs from a clone in that a connection 
+# exists between your fork and the original repository itself. In this way, your fork acts as a bridge 
+# between the original repository and your personal copy where you can contribute back to the original 
+# project using Pull Requests.
+#Forking a project is as easy as clicking the Fork button in the header of a repository. Once the process 
+# is complete, you'll be taken right to your the forked copy of the project so you can start collaborating!
+
+
+#3.3 Init VS Clone
+#git-init - Create an empty Git repository or reinitialize an existing one
+
+#Cloning
+#When you create a new repository on GitHub, it exists as a remote location where your project is stored.
+#You can clone your repository to create a local copy on your computer so that you can sync between both
+#the local and remote locations of the project.
+#Unlike forking, you won't be able to pull down changes from the original repository you cloned from, 
+#and if the project is owned by someone else you won't be able to contribute back to it unless you are 
+#specifically invited as a collaborator. Cloning is ideal for instances when you need a way to quickly 
+#get your own copy of a repository where you may not be contributing to the original project.
+
+#3.4 Branching and Merging
+#git-branch - List, create, or delete branches
+#git-merge - Join two or more development histories together
 
 # 4.Create a Git Repositories using CLI and Console make sure to give them a name AssingmentUpload_Through_CLI and 
 # AssignmentUpload_Through_Console respectively.cd 
